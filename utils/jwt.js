4c939a580f97dd135b4579ced312d074a099ef2c
saurabhwebdev <saurabhgmatprep@gmail.com>
2025-10-13 16:20:49 +0530

    Fix JWT expiration with default value

--- utils/jwt.js
+++ utils/jwt.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 // Generate JWT Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: process.env.JWT_EXPIRE || '30d' // Default to 30 days
   });
 };
 
