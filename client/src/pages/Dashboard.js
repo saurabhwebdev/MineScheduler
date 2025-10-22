2250e87222355e3725caaca8050628226d958e0e
saurabhwebdev <saurabhgmatprep@gmail.com>
2025-10-22 11:26:28 +0530

    Connect Dashboard component to /dashboard route in App.js

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../components/MainLayout';
+import DashboardComponent from '../components/Dashboard';
+
+const Dashboard = () => {
+  return (
+    <MainLayout>
+      <DashboardComponent />
+    </MainLayout>
+  );
+};
+
+export default Dashboard;
